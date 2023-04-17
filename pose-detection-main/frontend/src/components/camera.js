470b5f34f962236323e77133398b84f42d206e90
TheExamineer <97946101+TheExamineer@users.noreply.github.com>
2023-04-18 00:29:45 +0530

    minor changes in script of package.jsom
    server was showing no build script found

---
+++ pose-detection-main/frontend/src/components/camera.js
@@ -0,0 +1,31 @@
+navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+  .then((stream) => {
+    let mediaRecorder = new MediaRecorder(stream);
+    let chunks = [];
+
+    mediaRecorder.ondataavailable = function(event) {
+      chunks.push(event.data);
+    };
+
+    mediaRecorder.onstop = function() {
+      let videoBlob = new Blob(chunks, { type: 'video/mp4' });
+      let formData = new FormData();
+      formData.append('video', videoBlob);
+
+      fetch('https://yourbackendurl.com/upload', {
+        method: 'POST',
+        body: formData
+      })
+      .then(response => {
+        console.log('Video sent successfully!');
+      })
+      .catch(error => {
+        console.error('Error sending video:', error);
+      });
+    };
+
+    mediaRecorder.start();
+  })
+  .catch((err) => {
+    console.log("Error:", err);
+  });
